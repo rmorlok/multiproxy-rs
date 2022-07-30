c573442b07a6c5d420e955a12c7a2e3d36d3d470
Ryan Morlok <ryan.morlok@morlok.com>
2022-07-29 23:04:08 -0500

    At least compiling for TLS support from files

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,132 @@
 // Based on sample from:
 // https://github.com/actix/examples/tree/master/http-proxy
 
-use std::{fs, process};
-use clap::{Parser, CommandFactory};
 use actix_web::{error, middleware, web, App, HttpServer, Error, HttpRequest, HttpResponse};
 use awc::Client;
 use awc::http::StatusCode;
 use url::Url;
+use clap::{Parser, CommandFactory};
+use std::{fs, process};
+use std::fs::File;
+use std::io::BufReader;
+use rustls::{ServerConfig};
 
 #[derive(Parser)]
 #[clap(name = "multiproxy")]
 #[clap(author = "Ryan Morlok <ryan.morlok@morlok.com>")]
 #[clap(version)]
 #[clap(about = "Proxies requests to a configurable set of fallback hosts", long_about = None)]
-struct Cli {
+pub struct Cli {
     #[clap(parse(from_os_str), long = "pemPath")]
-    pem_path: Option<std::path::PathBuf>,
+    pub pem_path: Option<std::path::PathBuf>,
 
     #[clap(parse(from_os_str), long = "keyPath")]
-    key_path: Option<std::path::PathBuf>,
+    pub key_path: Option<std::path::PathBuf>,
 
     #[clap(long = "protocol", default_value_t = String::from("http"))]
-    protocol: String,
+    pub protocol: String,
 
     #[clap(long = "bindIp", default_value_t = String::from("0.0.0.0"))]
-    bind_ip: String,
+    pub bind_ip: String,
 
     #[clap(value_parser = clap::value_parser!(u16).range(1..65535), long = "port", default_value_t = 8888)]
-    port: u16,
+    pub port: u16,
 
     #[clap(value_parser,)]
-    forward_base_urls: Vec<String>
+    pub forward_base_urls: Vec<String>
+}
+
+impl Cli {
+    fn validate_path_param(path: &std::path::Path, name: &str) -> () {
+        let metadata_result = fs::metadata(&path);
+        if !metadata_result.is_ok() || !metadata_result.unwrap().is_file() {
+            eprintln!("--{} '{}' is not a valid file", name, path.to_string_lossy());
+            process::exit(1);
+        }
+    }
+
+    pub fn validate_forward_base_urls(&self) -> () {
+        if self.forward_base_urls.len() < 1 {
+            eprintln!("Must specify at least one proxy base url\n");
+            Cli::command().print_help().expect("failed to print usage");
+            process::exit(1);
+        }
+    }
+
+    pub fn validate_tls_params(&self) -> () {
+        if self.key_path.is_some() != self.pem_path.is_some() {
+            eprintln!("keyPath and pemPath must both be specified to enable TLS\n");
+            Cli::command().print_help().expect("failed to print usage");
+            process::exit(1);
+        }
+
+        if let Some(path) = self.key_path.clone() {
+            Cli::validate_path_param(&path, "keyPath");
+        }
+
+        if let Some(path) = self.pem_path.clone() {
+            Cli::validate_path_param(&path, "pemPath");
+        }
+    }
+
+    pub fn forward_base_urls(&self) -> Vec<Url> {
+        let mut forward_base_urls: Vec<Url> = vec![];
+
+        for url_string in &self.forward_base_urls {
+            let url = match Url::parse(&url_string) {
+                Ok(v) => v,
+                Err(_) => {
+                    eprintln!("Invalid base url '{}' specified", url_string);
+                    process::exit(1);
+                }
+            };
+
+            forward_base_urls.push(url);
+        }
+
+        forward_base_urls
+    }
+
+    fn read_certs_from_file(
+        key_path: std::path::PathBuf,
+        pem_path: std::path::PathBuf,
+    ) -> Result<(rustls::PrivateKey, Vec<rustls::Certificate>), Box<dyn std::error::Error>> {
+        let mut cert_chain_reader = BufReader::new(File::open(pem_path)?);
+        let certs = rustls_pemfile::certs(&mut cert_chain_reader)?
+            .into_iter()
+            .map(rustls::Certificate)
+            .collect();
+
+        let mut key_reader = BufReader::new(File::open(key_path)?);
+
+        // if the file starts with "BEGIN RSA PRIVATE KEY"
+        // let mut key_vec = rustls_pemfile::rsa_private_keys(&mut reader)?;
+        // if the file starts with "BEGIN PRIVATE KEY"
+        let mut keys = rustls_pemfile::pkcs8_private_keys(&mut key_reader)?;
+
+        assert_eq!(keys.len(), 1);
+        let key = rustls::PrivateKey(keys.remove(0));
+
+        Ok((key, certs))
+    }
+
+    pub fn rustls_server_config(&self) -> Result<Option<ServerConfig>, Box<dyn std::error::Error>> {
+        match [self.key_path.clone(), self.pem_path.clone()] {
+            [Some(key_path), Some(pem_path)] => {
+                let (key, cert_chain) = Cli::read_certs_from_file(key_path, pem_path)?;
+
+                return Ok(Some(ServerConfig::builder()
+                    .with_safe_default_cipher_suites()
+                    .with_safe_default_kx_groups()
+                    .with_safe_default_protocol_versions()
+                    .unwrap()
+                    .with_no_client_auth()
+                    .with_single_cert(cert_chain, key)
+                    .expect("bad certificate/key")));
+            }
+            _ => Ok(None)
+        }
+    }
 }
 
 async fn forward(
@@ -90,53 +186,16 @@ async fn forward(
     Err(error::ErrorNotFound("Not Found"))
 }
 
-fn validate_path_param(path: &std::path::Path, name: &str) -> () {
-    let metadata_result = fs::metadata(&path);
-    if !metadata_result.is_ok() || !metadata_result.unwrap().is_file() {
-        eprintln!("--{} '{}' is not a valid file", name, path.to_string_lossy());
-        process::exit(1);
-    }
-}
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
     let args: Cli = Cli::parse();
 
-    if args.forward_base_urls.len() < 1 {
-        eprintln!("Must specify at least one proxy base url\n");
-        Cli::command().print_help().expect("failed to print usage");
-        process::exit(1);
-    }
+    args.validate_forward_base_urls();
+    args.validate_tls_params();
 
-    if args.key_path.is_some() != args.pem_path.is_some() {
-        eprintln!("keyPath and pemPath must both be specified to enable TLS\n");
-        Cli::command().print_help().expect("failed to print usage");
-        process::exit(1);
-    }
-
-    if let Some(path) = args.key_path {
-        validate_path_param(&path, "keyPath");
-    }
-
-    if let Some(path) = args.pem_path {
-        validate_path_param(&path, "pemPath");
-    }
-
-    let mut forward_base_urls: Vec<Url> = vec![];
-
-    for url_string in args.forward_base_urls {
-        let url = match Url::parse(&url_string) {
-            Ok(v) => v,
-            Err(_) => {
-                eprintln!("Invalid base url '{}' specified", url_string);
-                process::exit(1);
-            }
-        };
-
-        forward_base_urls.push(url);
-    }
+    let forward_base_urls = args.forward_base_urls();
 
     log::info!(
         "starting HTTP server at http://{}:{}",
@@ -144,14 +203,28 @@ async fn main() -> std::io::Result<()> {
         args.port
     );
 
-    HttpServer::new(move || {
+    let s = HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(forward_base_urls.clone()))
             .app_data(web::Data::new(Client::default()))
             .wrap(middleware::Logger::default())
             .default_service(web::to(forward))
-    })
-        .bind((args.bind_ip, args.port))?
+    });
+
+    let s = match args.rustls_server_config() {
+        Ok(Some(config)) => {
+            s.bind_rustls((args.bind_ip, args.port), config).unwrap()
+        },
+        Ok(None) => {
+            s.bind((args.bind_ip, args.port)).unwrap()
+        },
+        Err(error) => {
+            eprintln!("Failed to configure for TLS: {}", error);
+            process::exit(1);
+        }
+    };
+
+    s
         .workers(2)
         .run()
         .await
