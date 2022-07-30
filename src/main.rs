84e3df5c641b45dc70578e6f49e5b846e000d3cf
Ryan Morlok <ryan.morlok@morlok.com>
2022-07-30 12:48:00 -0500

    Self-signed TLS working

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,9 @@ pub struct Cli {
     #[clap(parse(from_os_str), long = "keyPath")]
     pub key_path: Option<std::path::PathBuf>,
 
+    #[clap(long = "selfSignedTls", default_value_t = true)]
+    pub self_signed_tls: bool,
+
     #[clap(long = "protocol", default_value_t = String::from("http"))]
     pub protocol: String,
 
@@ -54,6 +57,12 @@ impl Cli {
     }
 
     pub fn validate_tls_params(&self) -> () {
+        if self.self_signed_tls && (self.key_path.is_some() || self.pem_path.is_some()) {
+            eprintln!("Cannot specify selfSignedTls in conjunction with keyPath and pemPath\n");
+            Cli::command().print_help().expect("failed to print usage");
+            process::exit(1);
+        }
+
         if self.key_path.is_some() != self.pem_path.is_some() {
             eprintln!("keyPath and pemPath must both be specified to enable TLS\n");
             Cli::command().print_help().expect("failed to print usage");
@@ -87,6 +96,13 @@ impl Cli {
         forward_base_urls
     }
 
+    fn generate_self_signed_cert(&self) -> Result<(rustls::PrivateKey, rustls::Certificate), Box<dyn std::error::Error>>
+    {
+        let cert = rcgen::generate_simple_self_signed(vec!["localhost".to_string(), self.bind_ip.to_string()])?;
+        let key = rustls::PrivateKey(cert.serialize_private_key_der());
+        Ok((key, rustls::Certificate(cert.serialize_der()?)))
+    }
+
     fn read_certs_from_file(
         key_path: std::path::PathBuf,
         pem_path: std::path::PathBuf,
@@ -111,8 +127,23 @@ impl Cli {
     }
 
     pub fn rustls_server_config(&self) -> Result<Option<ServerConfig>, Box<dyn std::error::Error>> {
-        match [self.key_path.clone(), self.pem_path.clone()] {
-            [Some(key_path), Some(pem_path)] => {
+        match (
+            self.self_signed_tls,
+            self.key_path.clone(),
+            self.pem_path.clone()
+        ) {
+            (true, _, _) => {
+                let (key, cert_chain) = self.generate_self_signed_cert()?;
+
+                return Ok(Some(ServerConfig::builder()
+                    .with_safe_default_cipher_suites()
+                    .with_safe_default_kx_groups()
+                    .with_safe_default_protocol_versions()
+                    .unwrap()
+                    .with_no_client_auth()
+                    .with_single_cert(vec![cert_chain], key)?));
+            }
+            (false, Some(key_path), Some(pem_path)) => {
                 let (key, cert_chain) = Cli::read_certs_from_file(key_path, pem_path)?;
 
                 return Ok(Some(ServerConfig::builder()
@@ -121,8 +152,7 @@ impl Cli {
                     .with_safe_default_protocol_versions()
                     .unwrap()
                     .with_no_client_auth()
-                    .with_single_cert(cert_chain, key)
-                    .expect("bad certificate/key")));
+                    .with_single_cert(cert_chain, key)?));
             }
             _ => Ok(None)
         }
@@ -197,12 +227,6 @@ async fn main() -> std::io::Result<()> {
 
     let forward_base_urls = args.forward_base_urls();
 
-    log::info!(
-        "starting HTTP server at http://{}:{}",
-        &args.bind_ip,
-        args.port
-    );
-
     let s = HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(forward_base_urls.clone()))
@@ -213,9 +237,19 @@ async fn main() -> std::io::Result<()> {
 
     let s = match args.rustls_server_config() {
         Ok(Some(config)) => {
+            log::info!(
+                "starting HTTP server at https://{}:{}",
+                &args.bind_ip,
+                args.port
+            );
             s.bind_rustls((args.bind_ip, args.port), config).unwrap()
         },
         Ok(None) => {
+            log::info!(
+                "starting HTTP server at https://{}:{}",
+                &args.bind_ip,
+                args.port
+            );
             s.bind((args.bind_ip, args.port)).unwrap()
         },
         Err(error) => {
